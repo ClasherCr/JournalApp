7901c0a5dc235fba83516c8ce28c31359650727e
Jashkumar Solanki <jashkumarsolanki@gmail.com>
2024-06-23 14:28:43 +0530

    User Setup

--- app/src/main/java/com/example/journalapp/MainActivity.java
+++ app/src/main/java/com/example/journalapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.journalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn, createAccountBtn;
+    private EditText emailEt, passEt;
+
+    // Firebase Auth
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        createAccountBtn = findViewById(R.id.creat_account);
+
+        createAccountBtn.setOnClickListener(v -> {
+            // Onclick()
+            Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(i);
+
+        });
+
     }
 }
